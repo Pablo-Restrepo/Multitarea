ee1f4195a02ceb474f116ded0bb9a34e1ce01b77
Pablo736 <restrepo736@gmail.com>
2023-06-18 22:49:09 -0500

    Added Multitarea code

--- Multitarea.ino
+++ Multitarea.ino
@@ -1,196 +1,127 @@
+/**
+ * Integrantes:
+ *
+ * Fabian Ome Peña
+ * Carlos Mario Perdomo Ramos
+ * Pablo Jose Restrepo Ruiz
+ *
+ * Arquitectura Computacional - Universidad del Cauca
+ *
+ * Fuentes:
+ *  Photoresistor: https://learn.sunfounder.com/lesson-21-photoresistor-sensor/
+ *  Humiture Sensor: https://learn.sunfounder.com/lesson-22-humiture-sensor/
+ */
+
 // Incluir las bibliotecas necesarias:
-#include <LiquidCrystal.h>
-#include <Keypad.h>
-
-// Definir el tamaño del teclado
-const byte ROWS = 4;
-const byte COLS = 4;
-
-// Definir la disposición de las teclas en el teclado
-char keys[ROWS][COLS] = {
-    {'1', '2', '3', 'A'},
-    {'4', '5', '6', 'B'},
-    {'7', '8', '9', 'C'},
-    {'*', '0', '#', 'D'}};
-
-// Definir los pines del Arduino conectados a las filas y columnas del teclado
-byte rowPins[ROWS] = {39, 41, 43, 45};
-byte colPins[COLS] = {47, 49, 51, 53};
-
-// Crear una instancia de Keypad con los parámetros anteriores
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+#include "AsyncTaskLib.h"  // Biblioteca para tareas asíncronas
+#include "DHTStable.h"     // Biblioteca para el sensor DHT
+#include <LiquidCrystal.h> // Biblioteca para la pantalla LCD
 
 // Definir los pines del Arduino conectados a la pantalla LCD
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-
 // Crear una instancia de LiquidCrystal para la pantalla LCD
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// Contraseña esperada: 1234
-char password[8] = {'1', '2', '3', '4'};
+DHTStable DHT;
+#define DHT11_PIN 6 // Pin al que esta conectado el SDA del sensor DHT11 al Arduino (pin 6)
 
-// Contraseña ingresada por el usuario
-char inputPassword[8];
+const int photocellPin = A0; // Pin analógico conectado al fotocélula
+const int ledPin = 13;       // Pin del LED
+int outputValue = 0;         // Valor de salida del fotocélula
 
-// Índice actual para ingresar la contraseña
-unsigned char idx = 0;
+void readTemperatureAndHumedity(); // Declaracion de los metodos para las multitareas
+void readLight();
 
-// Número de intentos de contraseña fallidos
-int attempts = 0;
-
-// Bandera para indicar si se han realizado tres intentos fallidos consecutivos
-boolean threeAttempts = false;
-
-// Definiciones de caracteres personalizados
-
-byte heart[8] = {
-    0b00000,
-    0b01010,
-    0b11111,
-    0b11111,
-    0b11111,
-    0b01110,
-    0b00100,
-    0b00000};
-
-byte smiley[8] = {
-    0b00100,
-    0b01110,
-    0b01110,
-    0b01110,
-    0b11111,
-    0b00000,
-    0b00100,
-    0b00000};
-
-byte frownie[8] = {
-    0b00000,
-    0b00000,
-    0b01010,
-    0b00000,
-    0b00000,
-    0b00000,
-    0b01110,
-    0b10001};
-
-byte armsDown[8] = {
-    0b00100,
-    0b01010,
-    0b00100,
-    0b00100,
-    0b01110,
-    0b10101,
-    0b00100,
-    0b01010};
-
-byte armsUp[8] = {
-    0b00100,
-    0b01010,
-    0b00100,
-    0b10101,
-    0b01110,
-    0b00100,
-    0b00100,
-    0b01010};
+AsyncTask asyncTaskReadTemperatureAndHumedity(2000, true, readTemperatureAndHumedity); // Tarea asíncrona para leer temperatura y humedad cada 2 segundos
+AsyncTask asyncTaskReadLight(4000, true, readLight);                                   // Tarea asíncrona para leer la luz cada 4 segundos
 
 /**
- * Inicializa el LCD, crea caracteres personalizados, muestra un mensaje de bienvenida
- y establece la posición inicial del cursor para ingresar la clave.
+ *@brief Configura la configuración inicial del programa.
+ *@details Este metodo inicializa la pantalla LCD, inicia la comunicación serial y ejecuta tareas asíncronas para leer la temperatura y humedad, así como para leer la luz.
+ *@details La pantalla LCD se configura con un tamaño de 16x2 caracteres. La comunicación serial se establece a una velocidad de baudios de 9600.
  */
 void setup()
 {
     lcd.begin(16, 2);
-    lcd.createChar(0, heart);
-    lcd.createChar(1, smiley);
-    lcd.createChar(2, frownie);
-    lcd.createChar(3, armsDown);
-    lcd.createChar(4, armsUp);
+    Serial.begin(9600);
 
-    lcd.setCursor(0, 0);
-
-    lcd.print("I ");
-    lcd.write(byte(0));
-    lcd.print(" Arduino! ");
-    lcd.write((byte)1);
-    delay(2000);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Ingrese Clave");
-    lcd.setCursor(0, 1);
+    asyncTaskReadTemperatureAndHumedity.Start();
+    asyncTaskReadLight.Start();
 }
 
 /**
- * Lee las teclas presionadas en el teclado numérico, verifica si se ha ingresado
- una clave correcta, imprime mensajes en el LCD y realiza acciones correspondientes
- en función de las teclas presionadas. Bloquea el sistema si hay más de 3 intentos fallidos.
+ *@brief Metodo principal del programa que se ejecuta en un ciclo infinito.
+ *@details Este metodo se ejecuta en un bucle infinito y actualiza las tareas asíncronas de lectura de temperatura y humedad, así como de lectura de luz.
  */
 void loop()
 {
-    char key = keypad.getKey();
-    if (attempts >= 3)
-    {
-        lcd.clear();
-        lcd.print("Siste. Bloqueado");
-        exit(0);
-    }
-    if (key)
-    {
-        if (key == '#')
-        {
-            if (isPasswordCorrect())
-            {
-                print("Clave Correcta!");
-                attempts = 0;
-                return;
-            }
-            print("Clave Incorrecta!");
-            attempts++;
-            return;
-        }
-        if (idx >= 8)
-        {
-            print("Fuera de Rango!");
-            return;
-        }
-        inputPassword[idx] = key;
-        lcd.print('*');
-        idx++;
-        (idx >= 8) ? idx : 0;
-    }
+    asyncTaskReadTemperatureAndHumedity.Update();
+    asyncTaskReadLight.Update();
 }
 
 /**
- * Imprime un mensaje en el LCD y restablece los valores.
- *
- * @param Message El mensaje a imprimir en el LCD.
+ *@brief Lee la temperatura y humedad del sensor DHT11.
+ *@details Este metodo lee la temperatura y humedad del sensor DHT11 y muestra los valores por el puerto serial y en la pantalla LCD.
  */
-void print(String Message)
+void readTemperatureAndHumedity()
 {
+    int chk = DHT.read11(DHT11_PIN);
+
+    Serial.print("Estado:\tHumedad (%):\tTemperatura (C):");
+    Serial.print("\n");
+
+    switch (chk)
+    {
+    case DHTLIB_OK:
+        Serial.print("OK\t");
+        break;
+    case DHTLIB_ERROR_CHECKSUM:
+        Serial.print("Checksum error\t");
+        break;
+    case DHTLIB_ERROR_TIMEOUT:
+        Serial.print("Time out error\t");
+        break;
+    default:
+        Serial.print("Unknown error\t");
+        break;
+    }
+
+    Serial.print(DHT.getHumidity());
+    Serial.print("\t\t");
+    Serial.print(DHT.getTemperature());
+    Serial.print("\t\t");
+    Serial.print("\n");
+
     lcd.clear();
-    lcd.print(Message);
+    lcd.setCursor(0, 0);
+    lcd.print("Humedad:");
+    lcd.setCursor(0, 1);
+    lcd.print(DHT.getHumidity());
     delay(2000);
     lcd.clear();
-    reset();
-}
 
-/**
- * Verifica si la contraseña ingresada es correcta.
- *
- * @return true si la contraseña es correcta, false de lo contrario.
- */
-bool isPasswordCorrect()
-{
-    return strcmp(inputPassword, password) == 0;
-}
-
-/**
- * Restablece los valores y muestra un mensaje en el LCD.
- */
-void reset()
-{
-    memset(inputPassword, 0, sizeof(inputPassword));
-    idx = 0;
     lcd.setCursor(0, 0);
-    lcd.print("Ingrese clave:");
+    lcd.print("Temperatura:");
     lcd.setCursor(0, 1);
+    lcd.print(DHT.getTemperature());
+    delay(2000);
+    lcd.clear();
+}
+
+/**
+ *@brief Lee la intensidad de luz del sensor de luz.
+ *@details Este metodo lee la intensidad de luz del sensor de luz y muestra el valor por el puerto serial y en la pantalla LCD.
+ */
+void readLight()
+{
+    outputValue = analogRead(photocellPin);
+
+    Serial.println("Luminosidad:");
+    Serial.println(outputValue);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Luminosidad:");
+    lcd.setCursor(0, 1);
+    lcd.print(outputValue);
 }
